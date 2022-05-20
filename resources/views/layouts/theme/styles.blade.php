2f57a5119fa4b2567160ccb9e7dd57df552ec556
Joan Tamayo <joan.tamayo@ese.centro>
2022-05-20 14:10:07 -0500

    initial commit

---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,50 @@
+
+<link href="assets/css/loader.css" rel="stylesheet" type="text/css" />
+<script src="assets/js/loader.js"></script>
+ <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+ <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+ <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+ <link href="assets/css/structure.css" rel="stylesheet" type="text/css" class="structure" />
+
+<link rel="stylesheet" href="{{asset('plugins/font-icons/fontawesome/css/fontawesome.css')}}" type="text/css"/>
+<link rel="stylesheet" href="{{asset('css/fontawesome.css')}}" type="text/css"/>
+
+<link rel="stylesheet" href="{{asset('assets/css/elements/avatar.css')}}" type="text/css"/>
+
+<link rel="stylesheet" href="{{asset('plugins/sweetalerts/sweetalert.css')}}" type="text/css"/>
+<link rel="stylesheet" href="{{asset('plugins/notification/snackbar/snackbar.min.css')}}" type="text/css"/>
+
+<link rel="stylesheet" href="{{asset('css/custom.css')}}" type="text/css" />
+
+<link rel="stylesheet" href="{{asset('assets/css/widgets/modules-widgets.css')}}" type="text/css" />
+<link rel="stylesheet" href="{{asset('assets/css/forms/theme-checkbox-radio.css')}}" type="text/css" />
+
+
+<style>
+    aside{ 
+        display: none !important;
+    }
+    .page-item.active .page-link{
+        z-index: 3;
+        color: #fff;
+        background-color: #3b3f5c;
+        border-color: #3b3f5c;
+    }
+    @media (max-width: 480px)
+    {
+        .mtmobile{
+            margin-bottom: 20px !important;
+        }
+        .mbmobile{
+            margin-bottom: 10px !important;
+        }
+        .hideonsm{
+            display: none !important;
+        }
+        .inblock{
+            display: block;
+        }
+    }
+</style>
+
+@livewireStyles
